f380d92b3cb7f1e449f5ce1169ffb8b1f9403545
ohgus <anakin1108@naver.com>
2023-06-09 16:36:15 +0900

    switch_conditional

---
+++ js/switch/app.js
@@ -0,0 +1,25 @@
+const day = 6;
+switch(day) {
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+    case 7:
+        console.log("weekend");
+        break;
+    default:
+        console.log("what it is!!");
+        break;
+}
