bf18a8a3d8cecaed13c71f31c3b088fb22fe15d4
ohgus <anakin1108@naver.com>
2023-06-09 15:30:25 +0900

    truthy & falsy

---
+++ js/truthy_falsy/app.js
@@ -0,0 +1,25 @@
+// const input = prompt("Enter something");
+
+// if(input) {
+//     console.log("truthy");
+// } else {
+//     console.log("falsy");
+// }
+
+// if(0) {
+//     console.log("truthy");
+// } else {
+//     console.log("falsy");
+// }
+
+// if(null) {
+//     console.log("truthy");
+// } else {
+//     console.log("falsy");
+// }
+
+if(NaN) {
+    console.log("truthy");
+} else {
+    console.log("falsy");
+}
