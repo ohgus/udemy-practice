b392c2236e2af47f80488b51941a8cfcb8de1b31
ohgus <anakin1108@naver.com>
2023-06-13 20:09:39 +0900

    make_method

---
+++ js/method/app.js
@@ -0,0 +1,11 @@
+const myMath = {
+    square(num){
+        return num * num;
+    },
+    cube(num){
+        return num ** 3;
+    }
+}
+
+console.log(myMath.cube(3)); // 27
+console.log(myMath.square(3)); // 9
