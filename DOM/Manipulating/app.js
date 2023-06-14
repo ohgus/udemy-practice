a7f3c6f860169381fb7ffa43a9409a6eb18af747
ohgus <anakin1108@naver.com>
2023-06-14 23:58:36 +0900

    dom_manipulating_practice

---
+++ DOM/Manipulating/app.js
@@ -0,0 +1,9 @@
+// document.querySelector('p').innerText = "lol";
+
+// const links = document.querySelectorAll("a");
+
+// for(let link of links){
+//     link.innerText = "I AM A LINK!!!";
+// }
+
+document.querySelector("h1").innerHTML = "<i>HELLO!</i>";
