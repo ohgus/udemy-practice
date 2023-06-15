2a13943be2ec1d9babdaf02a3f90ee5351f06d5d
ohgus <anakin1108@naver.com>
2023-06-15 17:41:19 +0900

    randomColorDemo_practice

---
+++ DOM_events/RandomColors/app.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector("button");
+const h1 = document.querySelector("h1");
+
+btn.addEventListener("click",() => {
+    const newColor = randomC();
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
+})
+
+const randomC = () => {
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `rgb(${r} , ${g} , ${b})`;
+}
