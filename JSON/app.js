850fe3ce885d239bd58b833c199b508aba08d630
ohgus <anakin1108@naver.com>
2023-06-16 22:45:29 +0900

    JSON parse

---
+++ JSON/app.js
@@ -0,0 +1,4 @@
+const data = `{"ticker":{"base":"BTC","target":"USD","price":"11288.49813464","volume":"91769.69699773","change":"-46.29462447"},"timestamp":1596510482,"success":true,"error":""}`
+
+const paesedData = JSON.parse(data);
+console.log(paesedData);
