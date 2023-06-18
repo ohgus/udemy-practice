58818e854bda7ba834c00d3225cb96c0f5178d5e
ohgus <anakin1108@naver.com>
2023-06-18 18:14:19 +0900

    new Function

---
+++ OOP/constructorFunction/app.js
@@ -0,0 +1,27 @@
+function Color(r,g,b){
+    this.r = r;
+    this.g = g;
+    this.b = b;
+}
+
+new Color(150,250,15);
+
+Color.prototype.rgb = function() { // 메서드 생성
+    const {r,g,b} = this;
+    return `rgb(${r}, ${g}, ${b})`;
+};
+Color.prototype.hex = function() {
+    const { r, g, b } = this;
+        return (
+            '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+        );
+};
+Color.prototype.rgba = function(a = 1.0) {
+    const {r,g,b} = this;
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+const color1 = new Color(40,255,60);
+const color2 = new Color(0,0,0);
+
+document.body.style.backgroundColor = color1.rgba(0.5);
