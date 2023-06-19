57b44200114ff293467329cf331ea6688d62a575
ohgus <anakin1108@naver.com>
2023-06-19 17:40:23 +0900

    fs & mkdir & writeFile

---
+++ Node_Intro/boilerplate.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const folderName = process.argv[2] || "Project";
+// fs.mkdir('Dogs', { recursive: true }, (err) => {
+//     console.log("IN THE CALLBACK!!");
+//     if (err) throw err;
+//   });
+try{
+    fs.mkdirSync(folderName);
+    fs.writeFileSync(`${folderName}/index.html`,"");
+    fs.writeFileSync(`${folderName}/app.js`,"");
+    fs.writeFileSync(`${folderName}/styles.css`,"");
+} catch (e) {
+    console.log("ERROR");
+    console.log(e);
+}
