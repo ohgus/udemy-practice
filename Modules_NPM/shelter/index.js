ebce5d3956c164592076405d91405302cd229a97
ohgus <anakin1108@naver.com>
2023-06-20 15:29:44 +0900

    package directory

---
+++ Modules_NPM/shelter/index.js
@@ -0,0 +1,8 @@
+const koko = require("./koko");
+const krong = require("./krong");
+const kiki = require("./kiki");
+
+const allLiz = [koko, krong, kiki];
+// console.log(allLiz);\
+
+module.exports = allLiz;
---
+++ Modules_NPM/shelter/kiki.js
@@ -0,0 +1,4 @@
+module.exports = {
+    name: "kiki",
+    color: "brown"
+}
