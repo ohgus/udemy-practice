e3a4ccba3bd6070f98d5fad699f628923d777367
ohgus <anakin1108@naver.com>
2023-06-21 17:16:08 +0900

    i reponse it

--- Express_Intro/FirstApp/index.js
+++ Express_Intro/FirstApp/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.use(() => {
+app.use((req, res) => {
     console.log("we got a new request!!");
+    // res.send("i am response!");
+    // res.send({
+    //     color: "red"
+    // })
+    res.send("<h1>THis is my response!</h1>");
 })
 
 app.listen(3000, () => {
