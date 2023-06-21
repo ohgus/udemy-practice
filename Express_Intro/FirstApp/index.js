7cf6f79adb17808e48e130d8709d7ad45ea90821
ohgus <anakin1108@naver.com>
2023-06-21 18:17:06 +0900

    make pattern for path

--- Express_Intro/FirstApp/index.js
+++ Express_Intro/FirstApp/index.js
@@ -14,6 +14,15 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("WELCOME HOME PAGE!!");
 })
+// make pattern
+app.get("/r/:subreddit",(req, res) => {
+    const {subreddit} = req.params;
+    res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`);
+})
+app.get("/r/:subreddit/:postID",(req, res) => {
+    const {subreddit, postID} = req.params;
+    res.send(`<h1>Viewing Post ID: ${postID} on the ${subreddit} subreddit</h1>`);
+})
 // /cats => "meow"
 app.get("/cats", (req, res) => {
     res.send("MEOW!!");
