08a813b40e96907126468e0849a2609310cc7676
ohgus <anakin1108@naver.com>
2023-06-22 22:38:58 +0900

    set path

--- Templating_Demo/index.js
+++ Templating_Demo/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 // ejs 파일을 불러오는 express 의 메서드
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views")); // 모든 디렉토리에서 views 폴더에 접근을 가능하게 해준다.
 
 app.get("/", (req, res) => {
     res.render("home");
