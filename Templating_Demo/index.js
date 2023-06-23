6e2010046f25936d15949b0f0346af022750a5fc
ohgus <anakin1108@naver.com>
2023-06-23 11:39:51 +0900

    make demo with ejs

--- Templating_Demo/index.js
+++ Templating_Demo/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const redditData = require("./data.json");
 
 // ejs 파일을 불러오는 express 의 메서드
 app.set("view engine", "ejs");
@@ -19,7 +20,12 @@ app.get("/cats", (req, res) => {
 
 app.get("/r/:subreddit",(req, res) => {
     const {subreddit} = req.params;
-    res.render("subreddit",{subreddit});
+    const data = redditData[subreddit];
+    if (data) {
+        res.render("subreddit",{ ...data});
+    } else {
+        res.render("notfound", { subreddit });
+    }
 })
 
 app.get("/random", (req, res) => {
