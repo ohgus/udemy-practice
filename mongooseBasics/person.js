83eb657926adb165a270b4ea1d9fe0d3a67111cd
ohgus <anakin1108@naver.com>
2023-06-27 17:27:10 +0900

    virtual mongoose

---
+++ mongooseBasics/person.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+mongoose.connect('mongodb://127.0.0.1:27017/shopApp')
+    .then(() => {
+        console.log("connection open!!");
+    })
+    .catch(err => {
+        console.log("oh no error!!");
+        console.log(err);
+    });
+
+const personSchema = new mongoose.Schema({
+    first: String,
+    last: String
+});
+
+personSchema.virtual("fullNmae").get(function () {
+    return `${this.first} ${this.last}`
+});
+
+const Person = mongoose.model("Person", personSchema);
