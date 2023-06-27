e92677dd2ef74faccbe4f3fd959ac9ef236d961e
ohgus <anakin1108@naver.com>
2023-06-27 23:54:51 +0900

    make route and index page

--- mongoose_express/index.js
+++ mongoose_express/index.js
@@ -19,8 +19,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/dog", (req, res) => {
-    res.send("woof!");
+app.get("/products", async (req, res) => {
+    const products = await Product.find({});
+    console.log(products);
+    res.render("products/index", {products});
 })
 
 app.listen(3000, () => {
