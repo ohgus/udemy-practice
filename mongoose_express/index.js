ed3fe2a33796c84dd6dc3ffce99a4d7fb08ff7a0
ohgus <anakin1108@naver.com>
2023-06-28 20:48:53 +0900

    product detail page & add product page

--- mongoose_express/index.js
+++ mongoose_express/index.js
@@ -18,13 +18,29 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
 
 app.get("/products", async (req, res) => {
     const products = await Product.find({});
-    console.log(products);
     res.render("products/index", {products});
+});
+
+app.get("/products/new", (req, res) => {
+    res.render("products/new");
+});
+
+app.post("/products", async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
 })
+
+app.get("/products/:id", async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.render("products/show", {product});
+});
 
 app.listen(3000, () => {
     console.log("app is listening on port 3000!");
-})
+});
