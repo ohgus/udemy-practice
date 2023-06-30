d0769c9011d871786263e9243adc3ac1ccfe5a0c
ohgus <anakin1108@naver.com>
2023-06-30 21:16:52 +0900

    make images in detail page

--- yelpcampPJ/seeds/index.js
+++ yelpcampPJ/seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000) + 1;
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "http://source.unsplash.com/collection/483251",
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, natus qui, veniam recusandae explicabo rem labore aut ex sunt unde, minus atque animi facilis nesciunt optio quisquam magnam debitis consequatur?",
+            price
         });
         await camp.save();
     }
