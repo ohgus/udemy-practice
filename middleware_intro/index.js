33e4dc28e13aa9ff5110aaddc7042b88138ca2d8
ohgus <anakin1108@naver.com>
2023-06-30 17:07:02 +0900

    make simple password demo

--- middleware_intro/index.js
+++ middleware_intro/index.js
@@ -15,6 +15,14 @@ app.use("/dogs", (req, res, next) => {
     next();
 });
 
+const verifyPassword = (req, res, next) => {
+    const {password} = req.query;
+    if(password === "sbk"){
+       return next(); // return 을 사용하지 않으면 서버가 클라이언트에 두개 이상의 응답을 보내려고 하여 에러가 발생한다.
+    }
+    res.send("SORRY U NEED A PASSWORD!");
+}
+
 // app.use((req, res, next) => {
 //     console.log("first middleware");
 //     next();
@@ -35,10 +43,14 @@ app.get("/dogs", (req, res) => {
     res.send("Woof Woof!");
 });
 
+app.get("/secret", verifyPassword, (req, res) => {
+    res.send("My Secret is: I will marry with sbk!");
+});
+
 app.use((req, res) => {
     res.status(404).send("NOT FOUND!");
-})
+});
 
 app.listen(3000, () => {
     console.log("App is running on Port 3000");
-})
+});
