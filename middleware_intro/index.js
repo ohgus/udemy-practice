6a233c2cea42aab37a6e44a2ffb0b45d4423258d
ohgus <anakin1108@naver.com>
2023-06-30 15:40:30 +0900

    decorate practice

--- middleware_intro/index.js
+++ middleware_intro/index.js
@@ -5,20 +5,28 @@ const morgan = require("morgan");
 app.use(morgan("tiny"));
 
 app.use((req, res, next) => {
-    console.log("first middleware");
-    next();
-    console.log("after middleware");
-});
-app.use((req, res, next) => {
-    console.log("second middleware");
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
     next();
 });
 
+// app.use((req, res, next) => {
+//     console.log("first middleware");
+//     next();
+//     console.log("after middleware");
+// });
+// app.use((req, res, next) => {
+//     console.log("second middleware");
+//     next();
+// });
+
 app.get("/", (req, res) => {
+    console.log(`REQUEST TIME: ${req.requestTime}`);
     res.send("Home Page!");
 });
 
 app.get("/dogs", (req, res) => {
+    console.log(`REQUEST TIME: ${req.requestTime}`);
     res.send("Woof Woof!");
 });
 
