bbca989b9e6d9e84f9db52e456a64775e1f54d76
ohgus <anakin1108@naver.com>
2023-07-03 16:37:18 +0900

    make util error handling function

--- async_error/index.js
+++ async_error/index.js
@@ -26,76 +26,63 @@ app.use(methodOverride("_method"));
 
 const categories = ["fruit", "vegetable", "dairy"];
 
-app.get("/products", async (req, res, next) => {
-    try{
-        const {category} = req.query;
-        if(category){
-            const products = await Product.find({category});
-            res.render("products/index", {products, category});
-        } else {
-            const products = await Product.find({});
-            res.render("products/index", {products, category: "All"});
-        }
-    } catch (e) {
-        next(e);
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(e => next(e));
     }
-});
+}
+
+app.get("/products", wrapAsync( async (req, res, next) => {
+    const {category} = req.query;
+    if(category){
+        const products = await Product.find({category});
+        res.render("products/index", {products, category});
+    } else {
+        const products = await Product.find({});
+        res.render("products/index", {products, category: "All"});
+    }
+}));
 
 app.get("/products/new", (req, res) => {
     res.render("products/new", {categories});
 });
 
-app.post("/products", async (req, res, next) => {
-    try{
-        const newProduct = new Product(req.body);
-        await newProduct.save();
-        res.redirect(`/products/${newProduct._id}`);
-    } catch (e) {
-        next(e);
-    }
-})
+app.post("/products", wrapAsync( async (req, res, next) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
+}));
 
-app.get("/products/:id", async (req, res, next) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        if(!product) {
-            throw next(new AppError("Product Not Found", 404));
-        }
-        res.render("products/show", {product});
-    } catch (e) {
-        next(e);
-    }
-});
 
-app.get("/products/:id/edit", async (req, res, next) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        if(!product) {
-            throw next(new AppError("Product Not Found", 404));
-        }
-        res.render("products/edit", {product, categories});
-    } catch (e) {
-        next(e);
+app.get("/products/:id", wrapAsync( async (req, res, next) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    if(!product) {
+        throw next(new AppError("Product Not Found", 404));
     }
-});
+    res.render("products/show", {product});
+}));
 
-app.put("/products/:id", async (req, res, next) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
-        res.redirect(`/products/${product._id}`);
-    } catch (e) {
-        next(e);
+app.get("/products/:id/edit", wrapAsync( async (req, res, next) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    if(!product) {
+        throw next(new AppError("Product Not Found", 404));
     }
-});
+    res.render("products/edit", {product, categories});
+}));
 
-app.delete("/products/:id", async (req, res) => {
+app.put("/products/:id",wrapAsync( async (req, res, next) => {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+    res.redirect(`/products/${product._id}`);
+}));
+
+app.delete("/products/:id",wrapAsync( async (req, res, next) => {
     const {id} = req.params;
     const productDelete = await Product.findByIdAndDelete(id);
     res.redirect("/products");
-});
+}));
 
 app.use((err, req, res, next) => {
     const{status = 500, message = "something went wrong"} = err;
