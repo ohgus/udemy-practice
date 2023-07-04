62fe1ae9ccb57fac2c2853c0c8e790e34828172b
ohgus <anakin1108@naver.com>
2023-07-04 23:28:55 +0900

    add product in to farm

--- mongoose_relationships_express/index.js
+++ mongoose_relationships_express/index.js
@@ -46,6 +46,22 @@ app.get("/farms/:id", async (req, res) => {
     res.render("farms/show", { farm });
 });
 
+app.get("/farms/:id/products/new", (req, res) => {
+    const { id } = req.params;
+    res.render("products/new", { categories, id });
+});
+
+app.post("/farms/:id/products", async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.send(farm);
+});
 // Product Routes
 
 const categories = ["fruit", "vegetable", "dairy"];
