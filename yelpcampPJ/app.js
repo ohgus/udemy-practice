bddf97a58523542bd5d4eb204c4ece1e52687378
ohgus <anakin1108@naver.com>
2023-07-05 18:54:30 +0900

    render reviews in show page

--- yelpcampPJ/app.js
+++ yelpcampPJ/app.js
@@ -67,7 +67,8 @@ app.post("/campgrounds", validateCampground, catchAsync(async (req, res) => {
 }));
 
 app.get("/campgrounds/:id", catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate("reviews");
+    console.log(campground);
     res.render("campgrounds/show", { campground });
 }));
 
