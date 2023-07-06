4b34adbc4a68282571a07d826faeb4ee8d2fc671
ohgus <anakin1108@naver.com>
2023-07-06 14:50:02 +0900

    cookie sign

--- router_cookies/cookieDemo/index.js
+++ router_cookies/cookieDemo/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const cookieParser = require("cookie-parser");
 
-app.use(cookieParser());
+app.use(cookieParser("singCookies"));
 
 app.get("/greet", (req, res) => {
     const {name = "No-Name"} = req.cookies;
@@ -14,6 +14,17 @@ app.get("/setname", (req, res) => {
     res.cookie("name", "Oh Leo");
     res.cookie("animal", "Jindo Dog");
     res.send("I SENT COOKIE!");
+});
+
+app.get("/fruitcookie", (req, res) => {
+    res.cookie("fruit", "apple", {signed: true});
+    res.send("signed cookie!");
+});
+
+app.get("/getsignedcookie", (req, res) => {
+    console.log(req.signedCookies);
+    console.log(req.cookies);
+    res.send(req.signedCookies);
 });
 
 app.listen(3000, () => {
