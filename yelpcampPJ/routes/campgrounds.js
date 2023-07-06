0e7919c59b65f19e8b25b3c1603873e8b0bd3b84
ohgus <anakin1108@naver.com>
2023-07-07 01:25:11 +0900

    add more flash and design it

--- yelpcampPJ/routes/campgrounds.js
+++ yelpcampPJ/routes/campgrounds.js
@@ -50,12 +50,14 @@ router.get("/:id/edit", catchAsync(async (req, res) => {
 router.put("/:id", validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash("success", "Success Update!");
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.delete("/:id", catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash("success", "Delete Campground!");
     res.redirect("/campgrounds");
 }));
 
