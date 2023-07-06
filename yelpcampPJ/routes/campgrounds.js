ec72d1e40a13535420b3a544a1bc82a00491f409
ohgus <anakin1108@naver.com>
2023-07-07 01:39:39 +0900

    make error flash

--- yelpcampPJ/routes/campgrounds.js
+++ yelpcampPJ/routes/campgrounds.js
@@ -39,11 +39,19 @@ router.post("/", validateCampground, catchAsync(async (req, res) => {
 
 router.get("/:id", catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate("reviews");
+    if(!campground) {
+        req.flash("error", "Can't find campground...");
+        return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/show", { campground });
 }));
 
 router.get("/:id/edit", catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if(!campground) {
+        req.flash("error", "Can't find campground...");
+        return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/edit", { campground });
 }));
 
