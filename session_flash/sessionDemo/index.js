db15bc25af00fb0bd0c81edfb0d4990f98870d84
ohgus <anakin1108@naver.com>
2023-07-06 20:06:35 +0900

    make session usefull

--- session_flash/sessionDemo/index.js
+++ session_flash/sessionDemo/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 
-app.use(session({secret: "thisisasecret"}));
+const sessionOptions = {secret: "thisisasecret", resave: false, saveUninitialized: false}
+
+app.use(session(sessionOptions));
 
 app.get("/viewcount", (req, res) => {
     if (req.session.count) {
@@ -11,6 +13,17 @@ app.get("/viewcount", (req, res) => {
         req.session.count = 1;
     }
     res.send(`you have viewed this page ${req.session.count} times`);
+});
+
+app.get("/register", (req, res) => {
+    const {username = "Anonymous"} = req.query;
+    req.session.username = username;
+    res.redirect("/greet");
+});
+
+app.get("/greet", (req, res) => {
+    const {username} = req.session;
+    res.send(`Welcome back, ${username}`);
 });
 
 app.listen(3000, () => {
