333f798d399a41fc9caadd31fee92d2a4829e4bf
ohgus <anakin1108@naver.com>
2023-07-06 11:04:55 +0900

    make middleware in router file

--- router_cookies/routerDemo/index.js
+++ router_cookies/routerDemo/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const shelterRoutes = require("./routes/shelters");
 const dogRoutes = require("./routes/dogs");
+const adminRoutes = require("./routes/admin");
 
 app.use("/dogs", dogRoutes);
 app.use("/shelters", shelterRoutes);
+app.use("/admin", adminRoutes);
 
 app.listen(3000, () => {
     console.log("In Port 3000!");
---
+++ router_cookies/routerDemo/routes/admin.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+router.use((req, res, next) => {
+    if(req.query.isAdmin){
+        next();
+    }
+    res.send("sorry not an admin");
+});
+
+router.get("/topsecret", (req, res) => {
+    res.send("This is top secret");
+});
+router.get("/delete", (req, res) => {
+    res.send("Ok delete!");
+});
+
+module.exports = router;
