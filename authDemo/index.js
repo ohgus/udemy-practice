f1d5ab4ba952c36aebea9e7562006ac2e808807b
ohgus <anakin1108@naver.com>
2023-07-10 21:48:55 +0900

    make login form and route

--- authDemo/index.js
+++ authDemo/index.js
@@ -38,6 +38,21 @@ app.post("/register", async (req, res) => {
     res.redirect("/");
 });
 
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+    const {username, password} = req.body;
+    const user = await User.findOne({username});
+    const validPassword = await bcrypt.compare(password, user.password);
+    if(validPassword) {
+        res.send("welcome!");
+    } else {
+        res.send("try again!");
+    }
+});
+
 app.get("/secret", (req, res) => {
     res.send("this is secret!");
 });
