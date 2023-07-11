a2a77386b893a79813c5e57852b66f3c35292bf8
ohgus <anakin1108@naver.com>
2023-07-11 22:19:32 +0900

    작성자에게만 리뷰 삭제권한 부여하기

--- yelpcampPJ/routes/campgrounds.js
+++ yelpcampPJ/routes/campgrounds.js
@@ -25,7 +25,13 @@ router.post("/", isLoggedIn, validateCampground, catchAsync(async (req, res) => 
 }));
 
 router.get("/:id", catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate("reviews").populate("author");
+    const campground = await Campground.findById(req.params.id).populate({
+        path: "reviews",
+        populate: {
+            path: "author"
+        }
+    }).populate("author");
+    console.log(campground);
     if(!campground) {
         req.flash("error", "Can't find campground...");
         return res.redirect("/campgrounds");
