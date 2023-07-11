89d5e22c0588f2bd0511d8e609536930aeb6bf54
ohgus <anakin1108@naver.com>
2023-07-11 16:53:09 +0900

    make logout route and add login, logout, register at navbar

--- yelpcampPJ/routes/users.js
+++ yelpcampPJ/routes/users.js
@@ -4,6 +4,8 @@ const passport = require("passport");
 const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
 
+// register
+
 router.get("/register", (req, res) => {
     res.render("users/register");
 });
@@ -21,6 +23,8 @@ router.post("/register", catchAsync(async(req, res) => {
     }
 }));
 
+// login
+
 router.get("/login", (req, res) => {
     res.render("users/login");
 });
@@ -28,6 +32,18 @@ router.get("/login", (req, res) => {
 router.post("/login", passport.authenticate("local", {failureFlash: true, failureRedirect: "/login"}), (req, res) => {
     req.flash("success", "Welcome Back!");
     res.redirect("/campgrounds");
+});
+
+// logout
+
+router.get("/logout", (req, res, next) => {
+    req.logOut(function (err) {
+        if(err) {
+            return next(err);
+        }
+        req.flash("success", "Goodbye!");
+        res.redirect("/campgrounds");
+    });
 });
 
 module.exports = router;
