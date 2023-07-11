fdbdc3b1f307a86f688f7fb254af1cbb09a1caf6
ohgus <anakin1108@naver.com>
2023-07-11 14:12:18 +0900

    connect passport

--- yelpcampPJ/app.js
+++ yelpcampPJ/app.js
@@ -6,7 +6,9 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const methodOverride = require("method-override");
 const ExpressError = require("./utils/ExpressError");
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
@@ -46,10 +48,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session()); // => 항상 세션 밑에 만들어야 함
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+});
+
+app.get("/fakeUser", async (req, res) => {
+    const user = new User({email: "omss@gmail.com", username: "oms"});
+    const newUser = await User.register(user, "monkey");
+    res.send(newUser);
 });
 
 app.use("/campgrounds", campgrounds);
