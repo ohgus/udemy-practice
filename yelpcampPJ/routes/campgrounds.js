83f10abc9db5e849986132c5864c9e0dadb49804
ohgus <anakin1108@naver.com>
2023-07-12 22:31:11 +0900

    connect cloudinary url to mongo

--- yelpcampPJ/routes/campgrounds.js
+++ yelpcampPJ/routes/campgrounds.js
@@ -11,11 +11,11 @@ const Campground = require("../models/campground");
 
 router.route("/")
     .get(catchAsync(campgrounds.index))
-    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-    .post(upload.array("image"),(req, res) => {
-        console.log(req.body, req.files);
-        res.send("it worked!!");
-    });
+    .post(isLoggedIn, upload.array("image"), validateCampground, catchAsync(campgrounds.createCampground));
+    // .post(upload.array("image"),(req, res) => {
+    //     console.log(req.body, req.files);
+    //     res.send("it worked!!");
+    // });
 
 router.get("/new",isLoggedIn, campgrounds.renderNewForm);
 
