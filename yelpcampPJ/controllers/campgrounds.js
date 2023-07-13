18e1b74e8e1437fa0c32dce0419fc95165838781
ohgus <anakin1108@naver.com>
2023-07-13 18:07:26 +0900

    use geoJson with mongoDB

--- yelpcampPJ/controllers/campgrounds.js
+++ yelpcampPJ/controllers/campgrounds.js
@@ -18,15 +18,14 @@ module.exports.createCampground = async (req, res) => {
         query: req.body.campground.location,
         limit: 1
     }).send();
-    res.send(geoData.body.features[0].geometry.coordinates);
-    // if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
-    // const campground = new Campground(req.body.campground);
-    // campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
-    // campground.author = req.user._id;
-    // await campground.save();
-    // console.log(campground);
-    // req.flash("success", "Success made a new campground!");
-    // res.redirect(`/campgrounds/${campground._id}`);
+    const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
+    campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
+    campground.author = req.user._id;
+    await campground.save();
+    console.log(campground);
+    req.flash("success", "Success made a new campground!");
+    res.redirect(`/campgrounds/${campground._id}`);
 }
 
 module.exports.showCampground = async (req, res) => {
--- yelpcampPJ/models/campground.js
+++ yelpcampPJ/models/campground.js
@@ -2,14 +2,31 @@ const mongoose = require("mongoose");
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual("thumbnail").get(function() {
+    return this.url.replace("/upload", "/upload/w_200");
+});
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
+    images: [ ImageSchema ],
+    // geoJson 을 이용하기 위한 로직
+    geometry: {
+        type: {
+            type: String,
+            enum: ["Point"],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
         }
-    ],
+    },
+    // --------------------------
     price: Number,
     description: String,
     location: String,
