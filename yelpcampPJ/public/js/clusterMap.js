afbe816a6c39657ba007c860ffb7f43d3cecd0d5
ohgus <anakin1108@naver.com>
2023-07-14 17:38:31 +0900

    add controller in map

--- yelpcampPJ/public/js/clusterMap.js
+++ yelpcampPJ/public/js/clusterMap.js
@@ -6,6 +6,9 @@ style: 'mapbox://styles/mapbox/light-v11',
 center: [-103.5917, 40.6699],
 zoom: 3
 });
+
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-right');
  
 map.on('load', () => {
     // Add a new source from our GeoJSON data and
--- yelpcampPJ/public/js/showPageMap.js
+++ yelpcampPJ/public/js/showPageMap.js
@@ -1,11 +1,21 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
       container: 'map', // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      style: 'mapbox://styles/mapbox/light-v11', // style URL
       center: campground.geometry.coordinates, // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: 13, // starting zoom
 });
 
-const marker = new mapboxgl.Marker({color: "black", rotation: 45})
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-right');
+
+
+const marker = new mapboxgl.Marker({color: "red", rotation: 45})
     .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+      new mapboxgl.Popup({offset: 25})
+      .setHTML(
+            `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
+    )
     .addTo(map);
